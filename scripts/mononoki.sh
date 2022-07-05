3d025ee1ff53764cb6d94512c8863c164d4df821
Jabybaby <jabybaby@protonmail.com>
2022-07-05 23:47:10 +0200

    Removed scripts folder

--- scripts/mononoki.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./yay.sh
-yay -S nerd-fonts-mononoki
-
-echo "font:
-  normal:
-    family: Mononoki Nerd Font
-    style: Regular" >> ~/.alacritty.yml
--- scripts/yay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pacman -S --needed  base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
